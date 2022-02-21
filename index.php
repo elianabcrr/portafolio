c5c9eae07b0fad1650849d5b51bbf2dca90d4a61
elianabcrr <eliana.becerra0916@gmail.com>
2022-02-21 11:53:52 -0400

    page structure

---
+++ index.php
@@ -0,0 +1,221 @@
+<?php
+    $skills = array(
+        array( 'skill-name' => 'JavaScript', 'skill-con' => '<i class="fa-brands fa-js"></i>'),
+        array( 'skill-name' => 'HTML5', 'skill-con' => '<i class="fa-brands fa-html5"></i>'),
+        array( 'skill-name' => 'jQuery', 'skill-con' => '<i class="fa-brands fa-js"></i>'),
+        array( 'skill-name' => 'Git', 'skill-con' => '<i class="fa-brands fa-git"></i>'),
+        array( 'skill-name' => 'Github', 'skill-con' => '<i class="fa-brands fa-github"></i>'),
+        array( 'skill-name' => 'VS Code', 'skill-con' => '<i class="fa-brands fa-js-square"></i>'),
+        array( 'skill-name' => 'Php', 'skill-con' => '<i class="fa-brands fa-php"></i>'),
+        array( 'skill-name' => 'Responsive Websites', 'skill-con' => '<i class="fa-brands fa-js-square"></i>'),
+        array( 'skill-name' => 'Web Accessibility', 'skill-con' => '<i class="fa-brands fa-js-square"></i>'),
+        array( 'skill-name' => 'WordPress', 'skill-con' => '<i class="fa-brands fa-wordpress"></i>'),
+        array( 'skill-name' => 'vuejs', 'skill-con' => '<i class="fa-brands fa-vuejs"></i>'),
+        array( 'skill-name' => 'Shopify', 'skill-con' => '<i class="fa-brands fa-shopify"></i>'),
+        array( 'skill-name' => 'Bootstrap', 'skill-con' => '<i class="fa-brands fa-bootstrap"></i>')
+    );
+
+    $portfolios = array(
+        array( 'port-img' => 'developer.jpg', 'port-title' => 'Material Design', 'port-text' => 'mauris proin sed nibh magna ipsum sollicitudin urna lobortis eros in ac malesuada bibendum...', 'port-link'=>'https://freefrontend.com/jquery-parallax/#parallax-examples'),
+        array( 'port-img' => 'contact-info.jpg', 'port-title' => 'Material Design', 'port-text' => 'mauris proin sed nibh magna ipsum sollicitudin urna lobortis eros in ac malesuada bibendum...', 'port-link'=>'https://freefrontend.com/jquery-parallax/#parallax-examples'),
+        array( 'port-img' => 'adham-dannaway-designer-coder.jpg', 'port-title' => 'Material Design', 'port-text' => 'mauris proin sed nibh magna ipsum sollicitudin urna lobortis eros in ac malesuada bibendum...', 'port-link'=>'https://freefrontend.com/jquery-parallax/#parallax-examples'),
+        array( 'port-img' => 'pexels-photo-1181673.jpeg', 'port-title' => 'Material Design', 'port-text' => 'mauris proin sed nibh magna ipsum sollicitudin urna lobortis eros in ac malesuada bibendum...', 'port-link'=>'https://freefrontend.com/jquery-parallax/#parallax-examples'),
+        array( 'port-img' => 'developer.jpg', 'port-title' => 'Material Design', 'port-text' => 'mauris proin sed nibh magna ipsum sollicitudin urna lobortis eros in ac malesuada bibendum...', 'port-link'=>'https://freefrontend.com/jquery-parallax/#parallax-examples'),
+        array( 'port-img' => 'developer.jpg', 'port-title' => 'Material Design', 'port-text' => 'mauris proin sed nibh magna ipsum sollicitudin urna lobortis eros in ac malesuada bibendum...', 'port-link'=>'https://freefrontend.com/jquery-parallax/#parallax-examples'),
+        array( 'port-img' => 'contact.jpg', 'port-title' => 'Material Design', 'port-text' => 'mauris proin sed nibh magna ipsum sollicitudin urna lobortis eros in ac malesuada bibendum...', 'port-link'=>'https://freefrontend.com/jquery-parallax/#parallax-examples'),
+        array( 'port-img' => 'pexels-photo-1181673.jpeg', 'port-title', 'port-title'  => 'Material Design', 'port-text' => 'mauris proin sed nibh magna ipsum sollicitudin urna lobortis eros in ac malesuada bibendum...', 'port-link'=>'https://freefrontend.com/jquery-parallax/#parallax-examples'),
+        array( 'port-img' => 'developer.jpg', 'port-title' => 'Material Design', 'port-text' => 'mauris proin sed nibh magna ipsum sollicitudin urna lobortis eros in ac malesuada bibendum...', 'port-link'=>'https://freefrontend.com/jquery-parallax/#parallax-examples'),
+        
+    );
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.min.js" integrity="sha512-oy0NXKQt2trzxMo6JXDYvDcqNJRQPnL56ABDoPdC+vsIOJnU+OLuc3QP3TJAnsNKXUXVpit5xRYKTiij3ov9Qg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+    <script src="js/custom.js"></script>
+    
+
+</head>
+    
+<body>
+    <header class="absolute header">
+        <div class="row container">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-lg navbar-light pd-left-60 pd-right-60">
+                    <div class="container-fluid p-0">
+                        <div>
+                            <a class="navbar-brand navLogo" href="#"><img src="img/LogoFInalMErg.png" alt=""></a>
+                        </div>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                            <ul class="navbar-nav mb-2 mb-lg-0 d-flex justify-content-end">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#section">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#about">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#skills">Skills</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#skills">Portfolio</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#contact">Contact</a>
+                                </li>
+                            </ul>
+                    
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main class="content" >
+        <section id="section">
+            <div class="row container">
+                <div class="col-12">
+                    <div class="content-text pd-left-60 pd-right-60">
+                        <h2>HI THERE I'M</h2>
+                        <h1>Eliana Becerra</h1>
+                        <h3>UI/UX DESIGNER + DEVELOPER </h3>
+                        <p>I’m a professional UI/UX designer with front end development skills based in Lagos.</p>
+                        <div>
+                            <a href="#" class="cta cta-hero">
+                                <span>hire me</span>
+                                <svg width="13px" height="10px" viewBox="0 0 13 10">
+                                    <path d="M1,5 L11,5"></path>
+                                    <polyline points="8 1 12 5 8 9"></polyline>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="face" id="face" >
+                        <img id="face-img" class="face-img" src="img/adham-dannaway-designer-coder.jpg" alt="Adham Dannaway UI designer" style="display: none; opacity: 1;">
+                        <div id="designer-img" class="designer-img" style="display: block;"></div>
+                        <div id="coder-img" class="coder-img" style="display: block;"></div>
+                        <div id="designer-bg" class="designer-bg" ></div>
+                        <div id="coder-bg" class="coder-bg"></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="about" id="about">
+            <div class="row container section-about pd-left-60 pd-right-60 pd-top-bottom">
+                <div class="col-4 about-left">
+                    <h5 class="sub-about sub-title">ABOUT</h5>
+                    <h2 class="title-about title-section">Who Is Eliana?</h2>
+                    <img src="img/developer-.gif" alt="image profile" class="image-profile">
+                </div>
+                <div class="col-8 about-right">
+                    <h2 class="title-about-c">Who Is Eliana</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                    <h2 class="title-about-c">Who Is Eliana</h2>
+                    <p class="mb-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                    
+                    <a href="#" class="cta cta-about">
+                        <span>Download cv</span>
+                        <svg width="13px" height="10px" viewBox="0 0 13 10">
+                            <path d="M1,5 L11,5"></path>
+                            <polyline points="8 1 12 5 8 9"></polyline>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </section>
+        <section class="skills-tools" id="skills">
+            <div class="row container  pd-left-60 pd-right-60 pd-top-bottom">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12">
+                            <h5 class="sub-skills text-center  sub-title">SKILLS & TOOLS</h5>
+                            <h2 class="title-skills text-center title-section">Lorem Ipsum is simply dummy text </h2>
+                            <p class="text-center">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s:</p>
+                            <div class="space"></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                    <?php
+                        foreach($skills as $skill){
+                        ?>
+                            <div class="col">
+                                <div class="card-icon">
+                                    <div class="icon-c">
+                                        <?php echo $skill['skill-con']; ?>
+                                    </div>
+                                    <p class="text"><?php echo $skill['skill-name'] ; ?></p> 
+                                </div>
+                            </div>
+                    <?php } ?>
+                    </div>
+                         
+                                
+                </div>
+            </div>
+        </section> 
+        <section class="portfolio" id="portfolio">
+            <div class="row container section-portfolio pd-left-60 pd-right-60 pd-top-bottom">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12">
+                            <h5 class="sub-skills text-center  sub-title">Portfolio</h5>
+                            <h2 class="title-skills text-center title-section">Lorem Ipsum is simply dummy text </h2>
+                            <p class="text-center">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s:</p>
+                            <div class="space"></div>
+                        </div>
+                    </div>
+                    <div class=" row-card">
+                        <div class="col-card">
+                            <?php
+                                foreach($portfolios as $port){ ?>
+                                    <div class="cardview">
+                                        <img src="../img/<?php echo $port['port-img'];?>"/>
+                                        <h1 class="title-port"><?php echo $port['port-title'];?></h1>
+                                        <section class="content">
+                                        <h1><?php echo $port['port-title'];?></h1>
+                                            <p> <?php echo $port['port-text'];?></p>
+                                            <a class="read-more" href="<?php echo $port['port-link'];?>"><span>Read More</span></a>
+                                        </section>
+                                    </div>
+                            <?php }
+                            ?>
+                            
+                           
+                            
+                        </div>
+                    </div>
+                </div>
+                       
+            </div>
+        </section>
+      
+        <section id="contact" >
+            <div class="under-inner two pd-top-bottom">
+                <h5 class="sub-contact  sub-title text-center">Contact</h5>
+                <h2 class="title-contact text-center  title-section">Lorem Ipsum is simply dummy text </h2>
+                <p class="text-center">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s:</p>
+                <div class="icons-contact">
+                    <a href=""><i class="fa-brands fa-whatsapp"></i></a>
+                    <a href=""><i class="fa-brands fa-linkedin"></i></a>
+                    <a href=""><i class="fa-solid fa-envelope"></i></a>
+                </div>
+            </div>
+            
+        </section> 
+    </main>
+</body>
+</html>
